f39496daa8400a10ecf839ea690ceb36ccb200a3
M-Kokolich <matthewkokolich@gmail.com>
2023-04-14 19:49:33 +1000

    CICD seems to work!

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn App() -> Html {
 
     html! {
         <div>
-            <h1>{ "Fourth try" }</h1>
+            <h1>{ "Double Check" }</h1>
             <button {onclick}>{ "+1" }</button>
             <p>{ *counter }</p>
         </div>

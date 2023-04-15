bbf2fba454aa7dc17b06affa1643bc0eeafcc04d
M-Kokolich <M-Kokolich@users.noreply.github.com>
2023-04-15 03:40:12 +0000

    deploy: c4584143de26499f5b05d70703611d7333f6db72

--- rust-site-2b6be78ba2115063.js
+++ rust-site-2b6be78ba2115063.js
@@ -856,7 +856,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('rust-site-f64153cc502fbae6_bg.wasm', import.meta.url);
+        input = new URL('rust-site-2b6be78ba2115063_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
